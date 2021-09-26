ecc9933de0c0ba5e2cbc9e71a83d89988e9b61f3
Oleh <olehhaliak@gmail.com>
2021-09-26 17:43:57 +0300

    added Mark inner class, added method for adding marks

--- lab1/src/main/java/Enrollment.java
+++ lab1/src/main/java/Enrollment.java
@@ -1,33 +1,54 @@
 import lombok.Data;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Data
 @Slf4j
 public class Enrollment {
-    private Map<Date, Integer> receivedMarks = new HashMap<>();
+    private List<Mark> receivedMarks = new ArrayList<>();
 
     public Enrollment() {
         log.info("Enrollment was created");
     }
 
-    public float getAverageToDate(Date date) {
-        List<Integer> marksToDate = receivedMarks.entrySet().stream()
-                .filter(E -> E.getKey().compareTo(date) > 0)
-                .map(Map.Entry::getValue)
+    public float getAverageToDate(LocalDate date) {
+        List<Mark> marksToDate = receivedMarks.stream()
+                .filter(E->E.date.isBefore(date)||E.date.isEqual(date))
                 .collect(Collectors.toList());
         return getAverage(marksToDate);
     }
 
     public float getFinalMark() {
-        return getAverage(receivedMarks.values());
+        return getAverage(receivedMarks);
     }
 
-    private float getAverage(Collection<Integer> marks) {
-        return marks.stream().reduce(Integer::sum).get() / (float) receivedMarks.size();
+    public void addMark(Integer mark) {
+        addMark(LocalDate.now(), mark);
     }
 
+    public void addMark(LocalDate date, Integer mark) {
+        receivedMarks.add(new Mark(date, mark));
+        log.info("Added new Mark: " + mark + ";received at " + date);
+    }
+
+    private float getAverage(Collection<Mark> marks) {
+        int sum = 0;
+        for (Mark mark : marks) {
+            sum += mark.value;
+        }
+        return sum / (float) marks.size();
+    }
+
+    private class Mark {
+        public Mark(LocalDate date, Integer value) {
+            this.date = date;
+            this.value = value;
+        }
+
+        LocalDate date;
+        Integer value;
+    }
 }
